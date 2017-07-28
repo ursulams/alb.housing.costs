2c4d64938628704f23fe86f5870ad225ce248058
Ursula Kaczmarek <ursulakaczmarek@users.noreply.github.com>
2017-07-28 15:29:17 -0400

    Update global.R

--- shinyapp/global.R
+++ shinyapp/global.R
@@ -11,17 +11,20 @@ library(ggthemes)
 
 
 # read in table data
-zip.data <- read.csv("zip.data.csv", colClasses = c("GEOID" = "character", zip = "character"))
+zip.data <- read.csv("zip.data.csv", colClasses = c("GEOID" = "character", "zip" = "character"))
+variables <- zip.data %>% gather(var, value, c(4:5, 7:10, 13)) %>% 
+  select(-jobs.per.household, -jobs.within.45.drive)
 
-### create spatial polygon data frames for leaflet polygons
 # read in polygon data
 blocks <- readOGR("blocks.shp", layer = "blocks", GDAL1_integer64_policy = TRUE) 
 
 # choices for drop-downs
 factors <- c(
   "Monthly housing cost" = "housing.cost",
+  "Population" = "population",
   "Size of block group (acres)" = "acreage",
   "Annual household greehouse gas output from auto use (metric tons)" = "household.co2",
   "Percent of housing that is owner occupied" = "pct.owner.occupied",
+  "Number of low-wage workers living in block group" = "low.wage.workers.home",
   "Number of high-wage workers living in block group" = "hi.wage.workers.home",
-  "Jobs available with a 45-minute drive" = "jobs.within.45.drive")
+  "Jobs available with a 45-minute drive (weighted)" = "weighted.jobs.within.45.drive")
