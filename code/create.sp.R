f61b5b93bdf180a4e9628a29a3ea1263728d4ee1
Ursula Kaczmarek <ursulakaczmarek@users.noreply.github.com>
2017-07-28 15:32:09 -0400

    Update create.sp.R

--- code/create.sp.R
+++ code/create.sp.R
@@ -10,8 +10,7 @@ blockgrps <- readOGR("cb_2013_36_bg_500k.shp", layer = "cb_2013_36_bg_500k", GDA
 # create dataset to join to polygon
 blocks <- all.data %<>%
   distinct(id, .keep_all = TRUE) %>%
-  filter(!is.na(housing.cost)) %>%
-  gather(factors, values, c(2:7))
+  gather(factors, values, c(2:8, 11))
 
 # subset GIS data to exclude block groups not in housing dataset
 block.ids <- blocks$GEOID
@@ -22,10 +21,3 @@ spdf.blocks <- sp::merge(blockgrps, blocks, duplicateGeoms = TRUE)
 
 # write spatial object
 writeOGR(obj = spdf.blocks, dsn = "alb.housing.costs", layer = "blocks", driver="ESRI Shapefile")
-
-
-
-
-
-
-
