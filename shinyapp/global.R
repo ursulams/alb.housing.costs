d36fb7bd4e3c74bc57f6ff2fa9d2e3b8e5e64b1e
Ursula Kaczmarek <ursulakaczmarek@users.noreply.github.com>
2017-07-31 15:37:03 -0400

    Update global.R

--- shinyapp/global.R
+++ shinyapp/global.R
@@ -9,22 +9,17 @@ library(rgdal)
 library(htmltools)
 library(ggthemes)
 
-
 # read in table data
-zip.data <- read.csv("zip.data.csv", colClasses = c("GEOID" = "character", "zip" = "character"))
-variables <- zip.data %>% gather(var, value, c(4:5, 7:10, 13)) %>% 
-  select(-jobs.per.household, -jobs.within.45.drive)
+zip.data <- read.csv("zip.data.csv", stringsAsFactors = FALSE, colClasses = c("GEOID" = "character", "zip" = "character"))
 
 # read in polygon data
-blocks <- readOGR("blocks.shp", layer = "blocks", GDAL1_integer64_policy = TRUE) 
+blocks <- readOGR("blocks.shp", layer = "blocks", GDAL1_integer64_policy = TRUE)
 
 # choices for drop-downs
 factors <- c(
   "Monthly housing cost" = "housing.cost",
-  "Population" = "population",
-  "Size of block group (acres)" = "acreage",
   "Annual household greehouse gas output from auto use (metric tons)" = "household.co2",
+  "Housing units per acre" = "housing.units.per.acre",
   "Percent of housing that is owner occupied" = "pct.owner.occupied",
-  "Number of low-wage workers living in block group" = "low.wage.workers.home",
   "Number of high-wage workers living in block group" = "hi.wage.workers.home",
-  "Jobs available with a 45-minute drive (weighted)" = "weighted.jobs.within.45.drive")
+  "Jobs available with a 45-minute drive" = "jobs.within.45.drive")
